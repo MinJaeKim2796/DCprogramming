8c29e690f4369b5300998d02aeaecb2641996b07
MinJaeKim2796 <59433841+MinJaeKim2796@users.noreply.github.com>
2020-10-26 01:06:43 +0900

    Update response.py

--- Dockerfile_python/response.py
+++ Dockerfile_python/response.py
@@ -2,14 +2,24 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "http://swcon.khu.ac.kr"
-response = requests.get(url)
-soup = BeautifulSoup(response.text, "html.parser")
-item = soup.select_one("head > title").string
-print("Go to", url)
-print("Status Code :", response.status_code)
-if response.status_code == 200:
-    print("RESPONSE OK!")
-    print("Department :", item)
-else :
-    print("RESPONSE FAILURE...")
+print("HTTP RESPONSE TEST")
+while True:
+    url = input("Input TEST URL : ")
+    print("Go to", url)
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            print("RESPONSE OK!")
+        else :
+            print("RESPONSE FAILURE... Status Code is ", response.status_code)
+    except requests.exceptions.MissingSchema as err:
+        print("Error : Invalid URL")
+    cont = input("Do you want to continue? [Y/n] : ")
+    if cont == 'Y' or cont == 'y' :
+        continue
+    elif cont == 'N' or cont == 'n' :
+        print("EXIT...")
+        break
+    else :
+        print("Wrong Input! Program End.")
+        break
